f53260243b22721bad384b07691b7f40eccb33ca
fdblay <67843349+fdblay@users.noreply.github.com>
2025-06-06 16:14:19 +0000

    favicon updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Layout from './components/Layout/Layout'
 import { Route, Routes } from 'react-router-dom'
